5061c6b79a2910d7d95c3162506e063be2a6d208
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-01-15 14:20:55 +0900

    [level Lv.1] Title: 폰켓몬, Time: 2.07 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
@@ -1,23 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        for(int j=0; j<nums.length-1; j++)
-            for(int i=0; i<nums.length-1-j; i++){
-                if(nums[j]==nums[i+j+1])
-                    nums[i+j+1]=0;
-                
-            }
         
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==0)
-                continue;         
-            else
-                if(answer<(nums.length/2))
-                    answer++;
-                else
-                    break;
+        Map<Integer,Integer> p = new HashMap<>();
+        // 포켓몬 종류만 있는 해쉬맵 생성
+        for(int n : nums){
+            if(!p.containsKey(n)){
+                p.put(n,1);
+            }
         }
         
+        // 종류와 가질 수 있는 수 비교
+        if(p.size()>=nums.length/2) answer = nums.length/2;
+        else answer = p.size();
         return answer;
     }
 }
