a2f76bd974690119f01897d302ca568e986cf970
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-01-25 02:23:50 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String,Integer> c = new HashMap<>();
+        
+        for(String[] c_ : clothes){
+           if(!c.containsKey(c_[1])) 
+               c.put(c_[1],1);
+            else
+                c.replace(c_[1],c.get(c_[1])+1); 
+        }
+        
+        System.out.print(c);
+        
+        for(Integer nums : c.values()){
+            answer *= (nums+1);
+        }
+        
+        return answer-1;
+    }
+}
