b7e4effa2ec3944d33ab785fe4358d7821ba76c7
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-03-04 10:05:08 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -3,23 +3,20 @@
 with join_members as(
     SELECT USER_ID 
     from USER_INFO 
-    where joined between '2021-01-01' and '2021-12-31' ),    
-group_os as(
-    select YEAR(sales_date) YEAR, MONTH(sales_date) MONTH ,user_id
-    from ONLINE_SALE 
-    group by YEAR(sales_date) ,MONTH(sales_date), user_id
-)
+    where YEAR(JOINED) = 2021 )    
+# group_os as(
+#     select YEAR(sales_date) YEAR, MONTH(sales_date) MONTH ,user_id
+#     from ONLINE_SALE 
+#     group by YEAR(sales_date) ,MONTH(sales_date), user_id
+# )
 
 
 select 
-    YEAR,
-    MONTH ,
-    count(user_id) PUCHASED_USERS	,
-    if(ROUND(count(user_id)/(select count(*) from join_members),1) ,
-       ROUND(count(user_id)/(select count(*) from join_members),1),
-      0) as PUCHASED_RATIO
-from group_os 
+    YEAR(sales_date) YEAR,
+    MONTH(sales_date)  MONTH ,
+    count(distinct user_id) PUCHASED_USERS	,
+    ROUND(count(distinct user_id)/(select count(*) from join_members),1)  as PUCHASED_RATIO
+from ONLINE_SALE  
 where USER_ID in (select * from join_members)
-group by YEAR,MONTH
-order by YEAR, MONTH;  
-
+group by YEAR(sales_date) ,MONTH(sales_date)
+order by YEAR, MONTH;
