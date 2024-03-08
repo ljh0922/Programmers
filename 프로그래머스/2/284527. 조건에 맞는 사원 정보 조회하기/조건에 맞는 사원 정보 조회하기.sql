9214acb521b1df621c3b0ecbd98cf1db57fa812b
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-03-08 17:06:12 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,15 +1,10 @@
--- 코드를 작성해주세요
-with emp_total_score as(
-    select emp_no, sum(score) total_score
-    from hr_grade
-    group by emp_no)
-    
 select 
-    ET.total_score SCORE,
-    HE.EMP_NO,
-    HE.EMP_NAME,
-    HE.POSITION,
-    HE.EMAIL
-from HR_EMPLOYEES HE join emp_total_score ET on HE.emp_no = ET.emp_no
-where ET.total_score = (select max(total_score) from emp_total_score);
-
+    sum(hg.score) SCORE,
+    he.EMP_NO,
+    he.EMP_NAME,
+    he.POSITION,
+    he.EMAIL
+from hr_grade hg join hr_employees he on hg.emp_no = he.emp_no
+group by hg.emp_no
+order by SCORE desc
+limit 1;
