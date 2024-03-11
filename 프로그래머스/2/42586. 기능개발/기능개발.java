882977619a03b228e38c6d3c8e001690772a36e6
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-03-11 14:50:26 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,26 +3,23 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        
+        Queue<Integer> q = new LinkedList<>();
         int[] remainDays = new int[progresses.length];
         
         for(int i=0; i<progresses.length; i++){
              remainDays[i] = (100-progresses[i])%speeds[i]>0 ?  (100-progresses[i])/speeds[i]+1 : (100-progresses[i])/speeds[i];
         } 
         
-        int cnt = 0;
-        int max = remainDays[0];
+        
         for(int r : remainDays){
-            if(max<r){
-                max = r;
-                answer.add(cnt);
-                cnt = 1;
+            if(!q.isEmpty()&&q.peek()<r){
+                answer.add(q.size());
+                q.clear();
             }
-            else{
-                cnt++;
-            }
+            q.offer(r);
+            
         }
-        answer.add(cnt);
+        answer.add(q.size());
         
         return answer;
     }
