ebf1abfa45406e5cf85fbe6ffc45cbf2a27c66f5
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-06-10 21:46:22 +0900

    [level 2] Title: 가장 큰 수, Time: 2.10 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#qna) 
 
 ### 성능 요약
 
-메모리: 65.2 MB, 시간: 2.40 ms
+메모리: 78.1 MB, 시간: 2.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 10일 20:18:15
+2024년 06월 10일 21:46:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -7,28 +7,21 @@ class Solution {
         for (int n : numbers) {
             nums.add(String.valueOf(n));
         }
-        
+
         // 커스텀 Comparator를 사용하여 정렬
-        Collections.sort(nums, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                String order1 = s1 + s2;
-                String order2 = s2 + s1;
-                return order2.compareTo(order1); // 내림차순으로 정렬
-            }
-        });
+        nums.sort((s1, s2) -> (s2 + s1).compareTo(s1 + s2));
 
         // 정렬된 문자열을 하나의 문자열로 합침
         StringBuilder answer = new StringBuilder();
         for (String num : nums) {
             answer.append(num);
         }
-        
-        // "0000..." 처럼 0이 여러개 있는 경우를 처리
+
+        // "0000..." 처럼 0이 여러 개 있는 경우를 처리
         if (answer.charAt(0) == '0') {
             return "0";
         }
-        
+
         return answer.toString();
     }
 }
