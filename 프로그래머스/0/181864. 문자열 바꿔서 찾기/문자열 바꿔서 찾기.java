9bdcbd043de32edc0defafddb82850ef90efe5b3
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-07-09 20:56:58 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,14 +1,7 @@
+
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        
-        String revers = "";
-        for(int i=0; i<myString.length(); i++){
-            revers += myString.substring(i,i+1).equals("A")? "B":"A";
-        }
-        System.out.println(revers);
-        answer = revers.indexOf(pat)==-1? 0:1;
-        
-        return answer;
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }
