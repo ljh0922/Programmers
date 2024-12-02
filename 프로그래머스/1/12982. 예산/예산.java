3d62c28373fec0d17f4cd0dbd9b5695c96e4d199
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2024-12-02 19:33:43 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -5,14 +5,18 @@ class Solution {
         int answer = 0;
         int sum = 0;
         
+        //신청금 배열 정렬
         Arrays.sort(d);
         
+        // 작은 수 부터 누적
         for(int i=0; i<d.length; i++){
             sum += d[i];
             answer = i;
+            // i가 0부터 시작이므로 그냥 break
             if(sum>budget) {
                 break;
             }
+            // i가 0부터 시작이므로 예산에 딱 맞거나, 다 더해도 예산보다 작은경우 +1
             else if(sum==budget || i==d.length-1) {
                 answer++;
                 break;
