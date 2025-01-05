9452fe266b7a6fd14c88b420e5e72a23fe8f5536
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2025-01-05 22:03:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 201.05 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -10,24 +10,25 @@ class Solution {
             reportedNum_by_id.put(id,0);
         }
                        
-        // 신고 목록 중복제거
+        // 중복으로 신고한 경우 제거
         Set<String> report_set = new HashSet<>();
         for(String r : report){
             report_set.add(r);
         }
         
-         // id 별 신고한 유저
+         // id 별 신고한 유저 Map<신고한 유저id, 신고당한 유저id List>
         Map<String, List<String>> reportList_by_id = new HashMap<>();
         for(String id : id_list){
             reportList_by_id.put(id,new ArrayList<>());
         }
         
+        // report 신고한id, 신고당한id 분리
         for(String r : report_set){
             String[] r_ = r.split(" ");
             reportList_by_id.get(r_[0]).add(r_[1]);
         }
         
-        // 신고당한 횟수 count
+        // 유저별 신고 list 가져와, 신고당한 횟수 count
         for(List<String> r_list : reportList_by_id.values()){
             for(String r : r_list){
                  reportedNum_by_id.put(r, reportedNum_by_id.get(r) + 1);
@@ -39,6 +40,7 @@ class Solution {
         answer = new int[id_list.length];
         
         for(int i=0; i<id_list.length; i++){
+            // 유저별 신고 list에서 정지된 id만 count
             for(String reported_id : reportList_by_id.get(id_list[i])){
                 if(reportedNum_by_id.get(reported_id) >= k){
                     answer[i]++;
