66fcb8d5fb3ed7d73aafa3f699ca4d8e91efcb94
ljh0922 <135215855+ljh0922@users.noreply.github.com>
2025-01-05 21:57:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 228.78 ms, Memory: 176 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        
+        // id 별 신고당한 횟수
+        Map<String, Integer> reportedNum_by_id = new HashMap<>();
+        for(String id : id_list){
+            reportedNum_by_id.put(id,0);
+        }
+                       
+        // 신고 목록 중복제거
+        Set<String> report_set = new HashSet<>();
+        for(String r : report){
+            report_set.add(r);
+        }
+        
+         // id 별 신고한 유저
+        Map<String, List<String>> reportList_by_id = new HashMap<>();
+        for(String id : id_list){
+            reportList_by_id.put(id,new ArrayList<>());
+        }
+        
+        for(String r : report_set){
+            String[] r_ = r.split(" ");
+            reportList_by_id.get(r_[0]).add(r_[1]);
+        }
+        
+        // 신고당한 횟수 count
+        for(List<String> r_list : reportList_by_id.values()){
+            for(String r : r_list){
+                 reportedNum_by_id.put(r, reportedNum_by_id.get(r) + 1);
+            }
+        }
+        System.out.println(reportedNum_by_id.toString());
+        
+        // id 별 메일 받는 횟수
+        answer = new int[id_list.length];
+        
+        for(int i=0; i<id_list.length; i++){
+            for(String reported_id : reportList_by_id.get(id_list[i])){
+                if(reportedNum_by_id.get(reported_id) >= k){
+                    answer[i]++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
